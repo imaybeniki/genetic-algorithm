ab606d4491387d0ba1fd849acfa135ceb586bf3a
imaybeniki <nikkimay96@gmail.com>
2021-04-24 14:30:47 -0700

    working state of a basic algorithm plus descriptive comments for learning f sharp

---
+++ GeneticAlgorithmConsoleApp/Display.fs
@@ -0,0 +1,22 @@
+﻿module GeneticAlgorithmConsoleApp.Display
+
+open System
+open GeneticAlgorithm.Logic.World
+
+  let printCell char isLastCell =
+    if isLastCell then
+      printfn "%c" char
+    else
+      printf "%c" char
+
+  let displayWorld (world: World) =
+    printfn ""
+    for y in 1..world.MaxX do
+    for x in 1..world.MaxY do   
+      let char = world.GetCharacterAtCell(x, y)
+      printCell char (x = world.MaxX)
+
+  let getUserInput(): ConsoleKeyInfo =
+    printfn ""
+    printfn "Press R to regenerate or X to exit"  
+    Console.ReadKey(true)  
---
+++ GeneticAlgorithmConsoleApp/Main.fs
@@ -0,0 +1,29 @@
+﻿open System
+open GeneticAlgorithm.Logic.World
+open GeneticAlgorithmConsoleApp.Display
+
+let generateWorld randomizer =
+  new World(8, 8, randomizer)
+
+[<EntryPoint>]
+let main argv =
+  printfn "F# Console Application Tutorial by Matt Eland"
+
+  let randomizer = new Random()
+
+  let mutable simulating: bool = true
+  let mutable world = generateWorld(randomizer)
+
+  while simulating do
+    displayWorld world
+
+    let key = getUserInput()
+
+    Console.Clear()
+
+    match key.Key with
+    | ConsoleKey.X -> simulating <- false
+    | ConsoleKey.R -> world <- generateWorld(randomizer)
+    | _ -> printfn "Invalid input '%c'" key.KeyChar
+
+  0 // return an integer exit code
